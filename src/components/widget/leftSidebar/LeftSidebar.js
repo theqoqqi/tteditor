5879ae25f47c951ca0d08fcacd869de980d22307
Qoqqi <sazykin.georgii@gmail.com>
2023-08-08 16:23:05 +0300

    feat: Теперь LeftSidebar и RightSidebar используют TabbedSidebar

--- src/components/widget/leftSidebar/LeftSidebar.js
+++ src/components/widget/leftSidebar/LeftSidebar.js
@@ -1,12 +1,37 @@
 import styles from './LeftSidebar.module.css';
-import React from 'react';
-import classNames from 'classnames';
+import React, {useState} from 'react';
+import TabbedSidebar from '../../entities/tabbedSidebar/TabbedSidebar.js';
+import {BsArrow90DegLeft, BsFolderFill, BsGripVertical} from 'react-icons/bs';
+
+const allTabs = [
+    {
+        title: 'Список карт',
+        icon: <BsFolderFill />,
+        content: <b>Список карт</b>,
+    },
+    {
+        title: 'Палитра',
+        icon: <BsGripVertical />,
+        content: <b>Палитра</b>,
+    },
+    {
+        title: 'Изменения',
+        icon: <BsArrow90DegLeft />,
+        content: <b>Изменения</b>,
+    },
+];
 
 function LeftSidebar({}) {
-    return (
-        <div className={classNames(styles.leftSidebarContent)}>
+    let [selectedTab, setSelectedTab] = useState(0);
 
-        </div>
+    return (
+        <TabbedSidebar
+            contentClassName={styles.leftSidebarContent}
+            side='left'
+            selectedTab={selectedTab}
+            onSelect={setSelectedTab}
+            tabs={allTabs}
+        />
     );
 }
 
--- src/components/widget/rightSidebar/RightSidebar.js
+++ src/components/widget/rightSidebar/RightSidebar.js
@@ -1,12 +1,37 @@
 import styles from './RightSidebar.module.css';
-import React from 'react';
-import classNames from 'classnames';
+import React, {useState} from 'react';
+import TabbedSidebar from '../../entities/tabbedSidebar/TabbedSidebar.js';
+import {BsBorderOuter, BsGridFill, BsPlayCircleFill} from 'react-icons/bs';
+
+const allTabs = [
+    {
+        title: 'Элементы',
+        icon: <BsGridFill />,
+        content: <b>Элементы</b>,
+    },
+    {
+        title: 'Триггеры',
+        icon: <BsPlayCircleFill />,
+        content: <b>Триггеры</b>,
+    },
+    {
+        title: 'Карта',
+        icon: <BsBorderOuter />,
+        content: <b>Карта</b>,
+    },
+];
 
 function RightSidebar({}) {
-    return (
-        <div className={classNames(styles.rightSidebarContent)}>
+    let [selectedTab, setSelectedTab] = useState(0);
 
-        </div>
+    return (
+        <TabbedSidebar
+            contentClassName={styles.rightSidebarContent}
+            side='right'
+            selectedTab={selectedTab}
+            onSelect={setSelectedTab}
+            tabs={allTabs}
+        />
     );
 }
 
