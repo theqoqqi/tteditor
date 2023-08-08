da80749b7d7737854d9b1944ac4ff3a51ec0580f
Qoqqi <sazykin.georgii@gmail.com>
2023-08-08 17:15:03 +0300

    feat: Ребрендинг

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name='viewport'
           content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
     <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-    <title>Totem Tribe Editor</title>
+    <title>TTEditor</title>
     <link rel='stylesheet' href='css/reset.css' />
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css' />
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css' />
