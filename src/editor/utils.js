03b6d872e11b104966ab224a4b91791d0caaacd7
Qoqqi <sazykin.georgii@gmail.com>
2023-08-09 19:29:28 +0300

    feat: Удалена функция isInViewport из util.js, файл больше не зависит от jQuery

--- src/editor/utils.js
+++ src/editor/utils.js
@@ -14,16 +14,6 @@ Element.prototype.getNumericContentOf = function (selector, defaultValue = null)
 XMLDocument.prototype[''+'getTextContentOf'] = Element.prototype.getTextContentOf;
 XMLDocument.prototype[''+'getNumericContentOf'] = Element.prototype.getNumericContentOf;
 
-$.fn.isInViewport = function() {
-    let elementTop = $(this).offset().top;
-    let elementBottom = elementTop + $(this).outerHeight();
-
-    let viewportTop = $(window).scrollTop();
-    let viewportBottom = viewportTop + $(window).height();
-
-    return elementBottom > viewportTop && elementTop < viewportBottom;
-};
-
 function hexIntColorToColor(color) {
     if (!color) {
         return null;
