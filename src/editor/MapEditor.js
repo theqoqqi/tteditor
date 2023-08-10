c51a0f134c48384a6c62763ac97f7b056c1df4b1
Qoqqi <sazykin.georgii@gmail.com>
2023-08-10 15:20:14 +0300

    refactor: Из класса MapEditor извлечен класс LevelAccess, отвечающий за внесение изменений в уровень и получение доступа к его составляющим

--- src/editor/MapEditor.js
+++ src/editor/MapEditor.js
@@ -18,16 +18,15 @@ import LeftSidebarComponent from './components/sidebars/LeftSidebarComponent.js'
 import RightSidebarComponent from './components/sidebars/RightSidebarComponent.js';
 import LayerListComponent from './components/sidebars/tabs/LayerListComponent.js';
 import BrushComponent from './components/BrushComponent.js';
-import CommandExecutor from './util/CommandExecutor.js';
 import CommandListComponent from './components/CommandListComponent.js';
 import RemoveNodesCommand from './commands/map/RemoveNodesCommand.js';
-import DummyCommand from './commands/DummyCommand.js';
 import PaletteMapNodeContextMenuComponent from './components/menus/PaletteMapNodeContextMenuComponent.js';
-import CompositeObserver from './util/observables/CompositeObserver.js';
 import UINodeFactory from './UINodeFactory.js';
 import {downloadXml, reformatXml} from './util/xml.js';
+import LevelAccess from './map/LevelAccess.js';
+import DummyCommand from './commands/DummyCommand.js';
 
-export default class MapEditor {
+export default class MapEditor extends LevelAccess {
 
     static POINTER_MODE_SELECT = Symbol('MapEditor.POINTER_MODE_SELECT');
 
@@ -36,13 +35,12 @@ export default class MapEditor {
     static POINTER_MODE_SCROLL = Symbol('MapEditor.POINTER_MODE_SCROLL');
 
     constructor(context) {
+        super();
         this.context = context;
         this.uiNodeFactory = new UINodeFactory(context);
 
         this.reader = new MapReader(context);
         this.writer = new MapWriter(context);
-
-        this.commandExecutor = new CommandExecutor();
 
         this.mapComponent = new MapComponent(this);
 
@@ -70,12 +68,10 @@ export default class MapEditor {
 
         this.brushComponent = new BrushComponent(this);
 
-        this.map = null;
         this.currentLevelFilename = null;
         this.pointerMode = null;
 
         this.bindListeners();
-        this.createObservers();
         this.mapComponent.setAllLayersActive();
         this.setPointerMode(MapEditor.POINTER_MODE_SELECT);
         this.reloadDataFromServer();
@@ -127,96 +123,6 @@ export default class MapEditor {
                 this.brushComponent.clearBrush();
             }
         });
-    }
-
-    createObservers() {
-        let valueChangeObserver = (newValue, oldValue) => {
-            if (newValue !== oldValue) {
-                this.setLevelDirty();
-            }
-        };
-
-        let listUpdateObserver = () => {
-            this.setLevelDirty();
-        };
-
-
-
-        this.mapObservers = new CompositeObserver();
-
-        this.mapObservers.addPropertyObserver('options', valueChangeObserver);
-        this.mapObservers.addPropertyObserver('terrain', valueChangeObserver);
-        this.mapObservers.addPropertyObserver('width', valueChangeObserver);
-        this.mapObservers.addPropertyObserver('height', valueChangeObserver);
-        this.mapObservers.addPropertyObserver('startX', valueChangeObserver);
-        this.mapObservers.addPropertyObserver('startY', valueChangeObserver);
-        this.mapObservers.addPropertyObserver('playerBaseX', valueChangeObserver);
-        this.mapObservers.addPropertyObserver('playerBaseY', valueChangeObserver);
-        this.mapObservers.addPropertyObserver('nodes', valueChangeObserver);
-        this.mapObservers.addPropertyObserver('triggers', valueChangeObserver);
-        this.mapObservers.addListObserver('nodes', listUpdateObserver);
-        this.mapObservers.addListObserver('triggers', listUpdateObserver);
-
-        this.mapObservers.addPropertyObserver('options.id', valueChangeObserver);
-        this.mapObservers.addPropertyObserver('options.music', valueChangeObserver);
-        this.mapObservers.addPropertyObserver('options.coloring', valueChangeObserver);
-        this.mapObservers.addPropertyObserver('options.fowClearColor', valueChangeObserver);
-        this.mapObservers.addPropertyObserver('options.randomizers', valueChangeObserver);
-        this.mapObservers.addListObserver('options.randomizers', listUpdateObserver);
-
-
-
-        this.mapNodeObservers = new CompositeObserver();
-
-        this.mapNodeObservers.addPropertyObserver('x', valueChangeObserver);
-        this.mapNodeObservers.addPropertyObserver('y', valueChangeObserver);
-        this.mapNodeObservers.addPropertyObserver('radius', valueChangeObserver);
-        this.mapNodeObservers.addPropertyObserver('name', valueChangeObserver);
-        this.mapNodeObservers.addPropertyObserver('hint', valueChangeObserver);
-        this.mapNodeObservers.addPropertyObserver('owner', valueChangeObserver);
-        this.mapNodeObservers.addPropertyObserver('subId', valueChangeObserver);
-        this.mapNodeObservers.addPropertyObserver('group', valueChangeObserver);
-
-
-
-        this.randomizerObservers = new CompositeObserver();
-
-        this.randomizerObservers.addPropertyObserver('item', valueChangeObserver);
-        this.randomizerObservers.addPropertyObserver('count', valueChangeObserver);
-
-
-
-        this.triggerObservers = new CompositeObserver();
-
-        this.triggerObservers.addPropertyObserver('title', valueChangeObserver);
-        this.triggerObservers.addPropertyObserver('repeat', valueChangeObserver);
-        this.triggerObservers.addPropertyObserver('statements', valueChangeObserver);
-        this.triggerObservers.addListObserver('statements', listUpdateObserver);
-
-
-
-        let addAttachDetachObservers = (propertyName, propertyObserver, attachableObserver) => {
-            propertyObserver.addPropertyObserver(propertyName, (items, oldItems) => {
-                for (const item of oldItems ?? []) {
-                    attachableObserver.detachFrom(item);
-                }
-                for (const item of items ?? []) {
-                    attachableObserver.attachTo(item);
-                }
-            });
-
-            propertyObserver.addElementAddedObserver(propertyName, item => {
-                attachableObserver.attachTo(item);
-            });
-
-            propertyObserver.addElementRemovedObserver(propertyName, item => {
-                attachableObserver.detachFrom(item);
-            });
-        };
-
-        addAttachDetachObservers('nodes', this.mapObservers, this.mapNodeObservers);
-        addAttachDetachObservers('triggers', this.mapObservers, this.triggerObservers);
-        addAttachDetachObservers('options.randomizers', this.mapObservers, this.randomizerObservers);
     }
 
     handleMapNodeClick(mapNode, e) {
@@ -305,45 +211,6 @@ export default class MapEditor {
         });
     }
 
-    setMapPropertyValue(propertyName, value) {
-        let propertyHolder = this.getPropertyHolder(propertyName);
-
-        propertyHolder[propertyName] = value;
-    }
-
-    getMapPropertyValue(propertyName) {
-        let propertyHolder = this.getPropertyHolder(propertyName);
-
-        return propertyHolder[propertyName];
-    }
-
-    getPropertyHolder(propertyName) {
-        let source = this.mapOptionsComponent.getPropertySource(propertyName);
-
-        return source === 'map' ? this.map : this.map.options;
-    }
-
-    setMapNodePropertyValue(mapNode, propertyName, value) {
-        if (propertyName === 'x') {
-            this.setMapNodePosition(mapNode, +value, mapNode.y);
-            return;
-        }
-
-        if (propertyName === 'y') {
-            this.setMapNodePosition(mapNode, mapNode.x, +value);
-            return;
-        }
-
-        let numericProperties = ['x', 'y', 'radius'];
-
-        if (numericProperties.includes(propertyName)) {
-            mapNode[propertyName] = +value;
-
-        } else {
-            mapNode[propertyName] = value;
-        }
-    }
-
     async loadLevel(filename) {
         let mapXml = await this.context.loadXml(filename);
         let map = this.reader.readLevel(mapXml);
@@ -351,12 +218,6 @@ export default class MapEditor {
         this.commandExecutor.clear();
         this.commandListComponent.clearCommands();
         this.triggerListComponent.clearTriggers();
-
-        if (this.map) {
-            this.mapObservers.detachFrom(this.map);
-        }
-
-        this.map = map;
 
         this.currentLevelFilename = filename;
         this.levelListComponent.setSelectedFile(filename);
@@ -377,8 +238,7 @@ export default class MapEditor {
         this.mapComponent.setMap(map);
         this.mapComponent.setViewportCenter(map.startX ?? map.playerBaseX, map.startY ?? map.playerBaseY);
 
-        this.mapObservers.attachTo(map);
-        this.mapObservers.triggerForAll();
+        this.setMap(map);
 
         this.setLevelClear();
         this.executeCommand(this.createInitialCommand());
@@ -443,25 +303,8 @@ export default class MapEditor {
         return this.levelListComponent.isLevelDirty();
     }
 
-    setLevelDirty() {
-        this.levelListComponent.setLevelDirty();
-    }
-
-    setLevelClear() {
-        this.levelListComponent.setLevelClear();
-    }
-
     hasLoadedLevel() {
         return this.currentLevelFilename !== null;
-    }
-
-    setMapNodePosition(mapNode, x, y) {
-        mapNode.x = x;
-        mapNode.y = y;
-
-        if (this.context.shouldMapNodeAlignToGrid(mapNode)) {
-            this.context.alignNodeToGrid(mapNode);
-        }
     }
 
     mapPositionToViewportPosition(mapNode) {
@@ -517,70 +360,6 @@ export default class MapEditor {
             || this.paletteMapNodeContextMenuComponent.isOpened;
     }
 
-    executeCommand(command) {
-        this.commandExecutor.execute(command);
-    }
-
-    undoCommand() {
-        this.commandExecutor.undo();
-    }
-
-    redoCommand() {
-        this.commandExecutor.redo();
-    }
-
-    indexOfNode(node) {
-        return this.map.indexOfNode(node);
-    }
-
-    insertNode(node, index) {
-        this.map.insertNode(node, index);
-    }
-
-    addNode(mapNode) {
-        this.map.addNode(mapNode);
-    }
-
-    removeNode(mapNode) {
-        this.map.removeNode(mapNode);
-    }
-
-    indexOfTrigger(trigger) {
-        return this.map.indexOfTrigger(trigger);
-    }
-
-    insertTrigger(trigger, index) {
-        this.map.insertTrigger(trigger, index);
-    }
-
-    addTrigger(trigger) {
-        this.map.addTrigger(trigger);
-    }
-
-    removeTrigger(trigger) {
-        this.map.removeTrigger(trigger);
-    }
-
-    setTerrain(terrain) {
-        this.map.setTerrain(terrain);
-    }
-
-    indexOfRandomizer(randomizer) {
-        return this.map.options.indexOfRandomizer(randomizer);
-    }
-
-    insertRandomizer(randomizer, index) {
-        this.map.options.insertRandomizer(randomizer, index);
-    }
-
-    addRandomizer(randomizer) {
-        this.map.options.addRandomizer(randomizer);
-    }
-
-    removeRandomizer(randomizer) {
-        this.map.options.removeRandomizer(randomizer);
-    }
-
     getSelectedNodes() {
         return this.nodeListComponent.getSelectedNodes();
     }
@@ -609,10 +388,6 @@ export default class MapEditor {
 
     getContext() {
         return this.context;
-    }
-
-    getMap() {
-        return this.map;
     }
 
     createMapNodeFromElement(element) {
---
+++ src/editor/map/LevelAccess.js
@@ -0,0 +1,252 @@
+import CompositeObserver from '../util/observables/CompositeObserver.js';
+import CommandExecutor from '../util/CommandExecutor.js';
+import {getProperty} from 'dot-prop';
+
+export default class LevelAccess {
+
+    #mapPropertySources = {
+        width: 'map',
+        height: 'map',
+        startX: 'map',
+        startY: 'map',
+        playerBaseX: 'map',
+        playerBaseY: 'map',
+
+        id: 'map.options',
+        music: 'map.options',
+        coloring: 'map.options',
+        fowClearColor: 'map.options',
+        randomizers: 'map.options',
+    };
+
+    isDirty;
+
+    constructor() {
+        this.commandExecutor = new CommandExecutor();
+
+        this.map = null;
+
+        this.createObservers();
+    }
+
+    createObservers() {
+        let valueChangeObserver = (newValue, oldValue) => {
+            if (newValue !== oldValue) {
+                this.setLevelDirty();
+            }
+        };
+
+        let listUpdateObserver = () => {
+            this.setLevelDirty();
+        };
+
+
+
+        this.mapObservers = new CompositeObserver();
+
+        this.mapObservers.addPropertyObserver('options', valueChangeObserver);
+        this.mapObservers.addPropertyObserver('terrain', valueChangeObserver);
+        this.mapObservers.addPropertyObserver('width', valueChangeObserver);
+        this.mapObservers.addPropertyObserver('height', valueChangeObserver);
+        this.mapObservers.addPropertyObserver('startX', valueChangeObserver);
+        this.mapObservers.addPropertyObserver('startY', valueChangeObserver);
+        this.mapObservers.addPropertyObserver('playerBaseX', valueChangeObserver);
+        this.mapObservers.addPropertyObserver('playerBaseY', valueChangeObserver);
+        this.mapObservers.addPropertyObserver('nodes', valueChangeObserver);
+        this.mapObservers.addPropertyObserver('triggers', valueChangeObserver);
+        this.mapObservers.addListObserver('nodes', listUpdateObserver);
+        this.mapObservers.addListObserver('triggers', listUpdateObserver);
+
+        this.mapObservers.addPropertyObserver('options.id', valueChangeObserver);
+        this.mapObservers.addPropertyObserver('options.music', valueChangeObserver);
+        this.mapObservers.addPropertyObserver('options.coloring', valueChangeObserver);
+        this.mapObservers.addPropertyObserver('options.fowClearColor', valueChangeObserver);
+        this.mapObservers.addPropertyObserver('options.randomizers', valueChangeObserver);
+        this.mapObservers.addListObserver('options.randomizers', listUpdateObserver);
+
+
+
+        this.mapNodeObservers = new CompositeObserver();
+
+        this.mapNodeObservers.addPropertyObserver('x', valueChangeObserver);
+        this.mapNodeObservers.addPropertyObserver('y', valueChangeObserver);
+        this.mapNodeObservers.addPropertyObserver('radius', valueChangeObserver);
+        this.mapNodeObservers.addPropertyObserver('name', valueChangeObserver);
+        this.mapNodeObservers.addPropertyObserver('hint', valueChangeObserver);
+        this.mapNodeObservers.addPropertyObserver('owner', valueChangeObserver);
+        this.mapNodeObservers.addPropertyObserver('subId', valueChangeObserver);
+        this.mapNodeObservers.addPropertyObserver('group', valueChangeObserver);
+
+
+
+        this.randomizerObservers = new CompositeObserver();
+
+        this.randomizerObservers.addPropertyObserver('item', valueChangeObserver);
+        this.randomizerObservers.addPropertyObserver('count', valueChangeObserver);
+
+
+
+        this.triggerObservers = new CompositeObserver();
+
+        this.triggerObservers.addPropertyObserver('title', valueChangeObserver);
+        this.triggerObservers.addPropertyObserver('repeat', valueChangeObserver);
+        this.triggerObservers.addPropertyObserver('statements', valueChangeObserver);
+        this.triggerObservers.addListObserver('statements', listUpdateObserver);
+
+
+
+        let addAttachDetachObservers = (propertyName, propertyObserver, attachableObserver) => {
+            propertyObserver.addPropertyObserver(propertyName, (items, oldItems) => {
+                for (const item of oldItems ?? []) {
+                    attachableObserver.detachFrom(item);
+                }
+                for (const item of items ?? []) {
+                    attachableObserver.attachTo(item);
+                }
+            });
+
+            propertyObserver.addElementAddedObserver(propertyName, item => {
+                attachableObserver.attachTo(item);
+            });
+
+            propertyObserver.addElementRemovedObserver(propertyName, item => {
+                attachableObserver.detachFrom(item);
+            });
+        };
+
+        addAttachDetachObservers('nodes', this.mapObservers, this.mapNodeObservers);
+        addAttachDetachObservers('triggers', this.mapObservers, this.triggerObservers);
+        addAttachDetachObservers('options.randomizers', this.mapObservers, this.randomizerObservers);
+    }
+
+    setMap(map) {
+        if (this.map) {
+            this.mapObservers.detachFrom(this.map);
+        }
+
+        this.map = map;
+
+        this.mapObservers.attachTo(map);
+        this.mapObservers.triggerForAll();
+    }
+
+    setMapPropertyValue(propertyName, value) {
+        let propertyHolder = this.getPropertyHolder(propertyName);
+
+        propertyHolder[propertyName] = value;
+    }
+
+    getMapPropertyValue(propertyName) {
+        let propertyHolder = this.getPropertyHolder(propertyName);
+
+        return propertyHolder[propertyName];
+    }
+
+    getPropertyHolder(propertyName) {
+        let sourcePath = this.#mapPropertySources[propertyName];
+
+        return getProperty(this, sourcePath);
+    }
+
+    setMapNodePropertyValue(mapNode, propertyName, value) {
+        if (propertyName === 'x') {
+            this.setMapNodePosition(mapNode, +value, mapNode.y);
+            return;
+        }
+
+        if (propertyName === 'y') {
+            this.setMapNodePosition(mapNode, mapNode.x, +value);
+            return;
+        }
+
+        let numericProperties = ['x', 'y', 'radius'];
+
+        if (numericProperties.includes(propertyName)) {
+            mapNode[propertyName] = +value;
+
+        } else {
+            mapNode[propertyName] = value;
+        }
+    }
+
+    setLevelDirty() {
+        this.isDirty = true;
+    }
+
+    setLevelClear() {
+        this.isDirty = false;
+    }
+
+    setMapNodePosition(mapNode, x, y) {
+        mapNode.x = x;
+        mapNode.y = y;
+    }
+
+    executeCommand(command) {
+        this.commandExecutor.execute(command);
+    }
+
+    undoCommand() {
+        this.commandExecutor.undo();
+    }
+
+    redoCommand() {
+        this.commandExecutor.redo();
+    }
+
+    indexOfNode(node) {
+        return this.map.indexOfNode(node);
+    }
+
+    insertNode(node, index) {
+        this.map.insertNode(node, index);
+    }
+
+    addNode(mapNode) {
+        this.map.addNode(mapNode);
+    }
+
+    removeNode(mapNode) {
+        this.map.removeNode(mapNode);
+    }
+
+    indexOfTrigger(trigger) {
+        return this.map.indexOfTrigger(trigger);
+    }
+
+    insertTrigger(trigger, index) {
+        this.map.insertTrigger(trigger, index);
+    }
+
+    addTrigger(trigger) {
+        this.map.addTrigger(trigger);
+    }
+
+    removeTrigger(trigger) {
+        this.map.removeTrigger(trigger);
+    }
+
+    setTerrain(terrain) {
+        this.map.setTerrain(terrain);
+    }
+
+    indexOfRandomizer(randomizer) {
+        return this.map.options.indexOfRandomizer(randomizer);
+    }
+
+    insertRandomizer(randomizer, index) {
+        this.map.options.insertRandomizer(randomizer, index);
+    }
+
+    addRandomizer(randomizer) {
+        this.map.options.addRandomizer(randomizer);
+    }
+
+    removeRandomizer(randomizer) {
+        this.map.options.removeRandomizer(randomizer);
+    }
+
+    getMap() {
+        return this.map;
+    }
+}
