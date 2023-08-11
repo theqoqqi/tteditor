6a654d561cb1e84714a38818b4867a227a308e7a
Qoqqi <sazykin.georgii@gmail.com>
2023-08-12 00:47:28 +0300

    feat: Доработана вкладка LevelListTab
    Теперь при нажатии на уровень он начинает загружаться.
    Если уровень будет изменен, будет отображаться метка "изменен".
    При наличии несохраненного измененного уровня, переключиться на другой уровень нельзя.

--- src/widget/leftSidebar/ui/levelListTab/LevelListTab.js
+++ src/widget/leftSidebar/ui/levelListTab/LevelListTab.js
@@ -1,19 +1,32 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {LevelList} from '../../../../entities/levelList';
+import {useEditor, useEditorObserver} from '../../../../shared/editor';
+import {useAsync} from 'react-use';
 
-function LevelListTab({}) {
+function LevelListTab() {
+    let editor = useEditor();
     let [levels, setLevels] = useState([]);
     let [selected, setSelected] = useState(null);
+    let asyncLevelList = useAsync(() => editor.loadLevelList(), [editor]);
+    let isLevelDirty = useEditorObserver('isLevelDirty');
 
-    function onSelect(level) {
-        setSelected(level);
-    }
+    useEffect(() => {
+        setLevels(asyncLevelList.value);
+    }, [asyncLevelList]);
+
+    useEffect(() => {
+        if (selected) {
+            editor.loadLevel(selected.path)
+                .catch(e => console.error(e));
+        }
+    }, [editor, selected]);
 
     return (
         <LevelList
             levels={levels}
             selected={selected}
-            onSelect={onSelect}
+            onSelect={setSelected}
+            isSelectedLevelDirty={isLevelDirty}
         />
     );
 }
