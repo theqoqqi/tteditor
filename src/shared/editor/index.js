fa17464c3a7e1eecc15f0e101ff95979478df8f6
Qoqqi <sazykin.georgii@gmail.com>
2023-08-12 22:22:44 +0300

    refactor: В shared/editor добавлены экспорты для утилитных функций shared/editor/core/util

--- src/shared/editor/index.js
+++ src/shared/editor/index.js
@@ -9,6 +9,10 @@ import useMapObserver from './lib/hooks/useMapObserver.js';
 import useEditorObserver from './lib/hooks/useEditorObserver.js';
 import EditorProvider from './lib/provider.js';
 import createNodeMeshStyles from './lib/rendering/styling.js';
+import * as colorsUtils from './core/util/colors.js';
+import * as geometryUtils from './core/util/geometry.js';
+import * as matrixUtils from './core/util/matrix.js';
+import * as xmlUtils from './core/util/xml.js';
 
 export {
     useMap,
@@ -22,4 +26,8 @@ export {
     useEditorObserver,
     EditorProvider,
     createNodeMeshStyles,
+    colorsUtils,
+    geometryUtils,
+    matrixUtils,
+    xmlUtils,
 };
