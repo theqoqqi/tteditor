e322687be4488f185791984063d6730abe4a77b7
Qoqqi <sazykin.georgii@gmail.com>
2023-08-13 19:25:49 +0300

    feat: В компонент MarkerMesh добавлен пропс style

--- src/entities/node/ui/markerMesh/MarkerMesh.js
+++ src/entities/node/ui/markerMesh/MarkerMesh.js
@@ -8,20 +8,24 @@ MarkerMesh.propTypes = {
     variant: PropTypes.oneOf(['fallback', 'icon', 'area']),
     className: PropTypes.any,
     children: PropTypes.any,
+    style: PropTypes.object,
     meshXml: PropTypes.instanceOf(Element),
     size: PropTypes.number,
     sizeX: PropTypes.number,
     sizeY: PropTypes.number,
 };
 
-function MarkerMesh({ variant, className, children, meshXml, size, sizeX, sizeY }) {
+function MarkerMesh({ variant, className, style, children, meshXml, size, sizeX, sizeY }) {
     let width = sizeX ?? size ?? xmlUtils.getNumericContent(meshXml, 'width');
     let height = sizeY ?? size ?? xmlUtils.getNumericContent(meshXml, 'height');
 
-    let style = styling.createMarkerMeshStyles(width, height);
+    let meshStyle = styling.createMarkerMeshStyles(width, height);
 
     return (
-        <div className={classNames(styles.markerMesh, styles[variant], className)} style={style}>
+        <div
+            className={classNames(styles.markerMesh, styles[variant], className)}
+            style={{...meshStyle, ...style}}
+        >
             {children}
         </div>
     );
