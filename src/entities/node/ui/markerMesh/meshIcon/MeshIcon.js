6e432c414598f1aaea2d046fbe3bff7e94dcffb4
Qoqqi <sazykin.georgii@gmail.com>
2023-08-13 19:09:00 +0300

    feat: Добавлен компонент MeshIcon

---
+++ src/entities/node/ui/markerMesh/meshIcon/MeshIcon.js
@@ -0,0 +1,46 @@
+import styles from './MeshIcon.module.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+    BsCircle,
+    BsHouseFill,
+    BsLayersFill,
+    BsLockFill,
+    BsPersonFill,
+    BsRecord2,
+    BsSearch,
+    BsSquareFill,
+    BsStars,
+    BsTreeFill,
+    BsVolumeUp
+} from 'react-icons/bs';
+
+const ICONS_BY_TAGS = {
+    terrain: BsSquareFill,
+    landmark: BsLayersFill,
+    structure: BsTreeFill,
+    building: BsHouseFill,
+    unit: BsPersonFill,
+    item: BsSearch,
+    chest: BsLockFill,
+    magic: BsStars,
+    ambient: BsVolumeUp,
+    waypoint: BsRecord2,
+    area: BsCircle,
+};
+
+MeshIcon.propTypes = {
+    tag: PropTypes.string,
+};
+
+function MeshIcon({ tag }) {
+    let Icon = ICONS_BY_TAGS[tag];
+
+    return (
+        <div className={styles.meshIcon}>
+            {Icon && <Icon />}
+        </div>
+    );
+}
+
+export default MeshIcon;
