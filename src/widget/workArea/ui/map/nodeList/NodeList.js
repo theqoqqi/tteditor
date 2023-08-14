f1b802c696c77518fc3f6f26caf44b54a6749385
Qoqqi <sazykin.georgii@gmail.com>
2023-08-14 21:45:13 +0300

    feat: Добавлена возможность перемещать выделенные ноды с помощью стрелок и кнопок ctrl, shift и alt

--- src/widget/workArea/ui/map/nodeList/NodeList.js
+++ src/widget/workArea/ui/map/nodeList/NodeList.js
@@ -9,7 +9,7 @@ import {
 } from '../../../../../shared/editor';
 import {Node} from '../../../../../entities/node';
 import {useDispatch, useSelector} from 'react-redux';
-import {isHotkeyPressed} from 'react-hotkeys-hook';
+import {isHotkeyPressed, useHotkeys} from 'react-hotkeys-hook';
 
 function NodeList() {
     /** @type MapNode[] */
@@ -17,6 +17,32 @@ function NodeList() {
     let dispatch = useDispatch();
     let selectedMapNodes = useSelector(selectSelectedMapNodes);
     let onClickMapNode = useCallback(handleMapNodeClicked, [dispatch]);
+
+    useHotkeys('up, down, left, right', (e, handler) => {
+        let h = handler.keys.includes('right') - handler.keys.includes('left');
+        let v = handler.keys.includes('down') - handler.keys.includes('up');
+        let baseStepSize = 5;
+        let stepSize = 1;
+
+        if (isHotkeyPressed('shift')) {
+            stepSize *= baseStepSize;
+        }
+
+        if (isHotkeyPressed('ctrl')) {
+            stepSize *= baseStepSize;
+        }
+
+        if (!isHotkeyPressed('alt')) {
+            stepSize *= baseStepSize;
+        }
+
+        for (const mapNode of selectedMapNodes) {
+            mapNode.x += h * stepSize;
+            mapNode.y += v * stepSize;
+        }
+    }, {
+        ignoreModifiers: true,
+    }, [selectedMapNodes]);
 
     let isSelected = mapNode => selectedMapNodes?.includes(mapNode);
 
