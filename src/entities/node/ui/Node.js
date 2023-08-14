3bfa9a1f79a47de7424345f45296166aacef785a
Qoqqi <sazykin.georgii@gmail.com>
2023-08-14 15:54:25 +0300

    feat: В компоненты Node добавлены пропсы hidden, muted и highlight

--- src/entities/node/ui/Node.js
+++ src/entities/node/ui/Node.js
@@ -1,23 +1,33 @@
+import styles from './Node.module.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 import {MapNode, useEditorContext, useNodeXml, useObserver} from '../../../shared/editor';
 import ObjectNode from './objectNode/ObjectNode.js';
 import MarkerNode from './markerNode/MarkerNode.js';
+import classNames from 'classnames';
 
 Node.propTypes = {
     mapNode: PropTypes.instanceOf(MapNode),
     nodeXml: PropTypes.instanceOf(Document),
     selected: PropTypes.bool,
     onClick: PropTypes.func,
+    hidden: PropTypes.bool,
+    muted: PropTypes.bool,
+    highlight: PropTypes.bool,
 };
 
-function Node({ mapNode, selected, onClick }) {
+function Node({ mapNode, selected, onClick, hidden, muted, highlight }) {
     let editorContext = useEditorContext();
     let tag = useObserver(mapNode, 'tag');
     let type = useObserver(mapNode, 'type');
     let nodeXml = useNodeXml(tag, type);
 
     let nodeProps = {
+        className: classNames({
+            [styles.hidden]: hidden,
+            [styles.muted]: muted,
+            [styles.highlight]: highlight,
+        }),
         selected,
         onClick,
     };
