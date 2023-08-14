f19977d6faa9de9c1ab3fd90053fdeb6ea8d039a
Qoqqi <sazykin.georgii@gmail.com>
2023-08-14 20:24:13 +0300

    feat: Добавлена возможность выделить нод кликом по нему

--- src/widget/workArea/ui/map/nodeList/NodeList.js
+++ src/widget/workArea/ui/map/nodeList/NodeList.js
@@ -1,15 +1,31 @@
 import styles from './NodeList.module.css';
 import React from 'react';
-import {useMapObserver} from '../../../../../shared/editor';
+import {selectSelectedMapNodes, setSelection, useMapObserver} from '../../../../../shared/editor';
 import {Node} from '../../../../../entities/node';
+import {useDispatch, useSelector} from 'react-redux';
 
 function NodeList() {
     /** @type MapNode[] */
     let mapNodes = useMapObserver('nodes');
+    let dispatch = useDispatch();
+    let selectedMapNodes = useSelector(selectSelectedMapNodes);
+
+    let isSelected = mapNode => selectedMapNodes?.includes(mapNode);
+
+    function onClickMapNode(mapNode) {
+        dispatch(setSelection(mapNode));
+    }
 
     return (
         <div className={styles.mapNodeList}>
-            {mapNodes?.map(mapNode => <Node key={mapNode.editorId} mapNode={mapNode} />)}
+            {mapNodes?.map(mapNode => (
+                <Node
+                    key={mapNode.editorId}
+                    mapNode={mapNode}
+                    selected={isSelected(mapNode)}
+                    onClick={() => onClickMapNode(mapNode)}
+                />
+            ))}
         </div>
     );
 }
