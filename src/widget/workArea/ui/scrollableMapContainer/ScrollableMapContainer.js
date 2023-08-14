113fc75290aaf957449322692e909ffaeb815d23
Qoqqi <sazykin.georgii@gmail.com>
2023-08-15 01:29:04 +0300

    refactor: Компонент MapContainer объединен с ScrollableMapContainer

--- src/widget/workArea/ui/scrollableMapContainer/ScrollableMapContainer.js
+++ src/widget/workArea/ui/scrollableMapContainer/ScrollableMapContainer.js
@@ -2,7 +2,7 @@ import styles from './ScrollableMapContainer.module.css';
 import React from 'react';
 import ScrollContainer from 'react-indiana-drag-scroll';
 import {useMap} from '../../../../shared/editor';
-import MapContainer from '../mapContainer/MapContainer.js';
+import Map from '../map/Map.js';
 
 let MIDDLE_MOUSE_BUTTON = 1;
 
@@ -10,8 +10,10 @@ function ScrollableMapContainer() {
     let map = useMap();
 
     return (
-        <ScrollContainer className={styles.mapContainer} buttons={[MIDDLE_MOUSE_BUTTON]}>
-            {map && <MapContainer />}
+        <ScrollContainer className={styles.scrollableMapContainer} buttons={[MIDDLE_MOUSE_BUTTON]}>
+            <div className={styles.overscrollArea}>
+                {map && <Map />}
+            </div>
         </ScrollContainer>
     );
 }
