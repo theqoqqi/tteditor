b45c8b9f3a6c9f0441da94edd3014e49139a9bdf
Qoqqi <sazykin.georgii@gmail.com>
2023-08-14 15:34:50 +0300

    feat: В компонент Node добавлены пропсы selected и onClick

--- src/entities/node/ui/Node.js
+++ src/entities/node/ui/Node.js
@@ -7,23 +7,30 @@ import MarkerNode from './markerNode/MarkerNode.js';
 Node.propTypes = {
     mapNode: PropTypes.instanceOf(MapNode),
     nodeXml: PropTypes.instanceOf(Document),
+    selected: PropTypes.bool,
+    onClick: PropTypes.func,
 };
 
-function Node({ mapNode }) {
+function Node({ mapNode, selected, onClick }) {
     let editorContext = useEditorContext();
     let tag = useObserver(mapNode, 'tag');
     let type = useObserver(mapNode, 'type');
     let nodeXml = useNodeXml(tag, type);
 
+    let nodeProps = {
+        selected,
+        onClick,
+    };
+
     if (editorContext.isMarkerNode(tag)) {
-        return <MarkerNode mapNode={mapNode} />;
+        return <MarkerNode mapNode={mapNode} {...nodeProps} />;
     }
 
     if (!nodeXml) {
         return null;
     }
 
-    return <ObjectNode mapNode={mapNode} nodeXml={nodeXml} />;
+    return <ObjectNode mapNode={mapNode} nodeXml={nodeXml} {...nodeProps} />;
 }
 
 export default Node;
