176cbd689c3186a5c38fe2d31b37bf4fcb70b05c
Qoqqi <sazykin.georgii@gmail.com>
2023-08-17 00:39:44 +0300

    feat: Добавлена подсказка после скачивания файла

--- src/widget/mainToolbar/ui/level/DownloadLevelButton.js
+++ src/widget/mainToolbar/ui/level/DownloadLevelButton.js
@@ -2,14 +2,24 @@ import React from 'react';
 import {ToolbarIconButton} from '../../../../shared/ui';
 import {BsFileEarmarkArrowDownFill} from 'react-icons/bs';
 import {useDownloadLevel} from '../../../../features/level';
+import {useEditor} from '../../../../entities/editor';
 
 function DownloadLevelButton() {
+    let editor = useEditor();
     let downloadLevel = useDownloadLevel();
+
+    function onClick() {
+        let path = editor.loadedLevelPath;
+
+        downloadLevel();
+
+        alert(`В игре файл должен храниться здесь:\nИГРА/${path}`);
+    }
 
     return (
         <ToolbarIconButton
             icon={BsFileEarmarkArrowDownFill}
-            onClick={downloadLevel}
+            onClick={onClick}
         />
     );
 }
