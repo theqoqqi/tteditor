7e75a0ca7e2d37b2e7a1b862b248462cf6f24bf6
Qoqqi <sazykin.georgii@gmail.com>
2023-08-18 22:12:12 +0300

    chore: Из репозитория удалены все файлы legacyEditor

--- src/legacyEditor/views/menus/MapNodeContextMenuView.js
+++
@@ -1,8 +0,0 @@
-import ContextMenuView from '../util/ContextMenuView.js';
-
-export default class MapNodeContextMenuView extends ContextMenuView {
-
-    constructor() {
-        super();
-    }
-}
--- src/legacyEditor/views/menus/PaletteMapNodeContextMenuView.js
+++
@@ -1,8 +0,0 @@
-import ContextMenuView from '../util/ContextMenuView.js';
-
-export default class PaletteMapNodeContextMenuView extends ContextMenuView {
-
-    constructor() {
-        super();
-    }
-}
