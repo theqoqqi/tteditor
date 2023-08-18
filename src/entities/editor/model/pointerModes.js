2d7b414d7fa63f6d75fca182a836e713d073dc58
Qoqqi <sazykin.georgii@gmail.com>
2023-08-19 01:10:27 +0300

    feat: В pointerModes во все режимы добавлены поля scrollButtons и dragButton

--- src/entities/editor/model/pointerModes.js
+++ src/entities/editor/model/pointerModes.js
@@ -1,15 +1,22 @@
 import {BsArrowsMove, BsFullscreen} from 'react-icons/bs';
 
+let LEFT_MOUSE_BUTTON = 0;
+let MIDDLE_MOUSE_BUTTON = 1;
+
 const pointerModes = {
     select: {
         name: 'select',
         title: 'Режим выделения',
         icon: BsFullscreen,
+        scrollButtons: [MIDDLE_MOUSE_BUTTON],
+        dragButton: LEFT_MOUSE_BUTTON,
     },
     scroll: {
         name: 'scroll',
         title: 'Режим прокрутки',
         icon: BsArrowsMove,
+        scrollButtons: [LEFT_MOUSE_BUTTON, MIDDLE_MOUSE_BUTTON],
+        dragButton: null,
     },
 };
 
