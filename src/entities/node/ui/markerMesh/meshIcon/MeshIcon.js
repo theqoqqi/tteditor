3e53def4bdea997ab82dd157d57b0f363149f748
Qoqqi <sazykin.georgii@gmail.com>
2023-08-26 06:29:56 +0300

    feat: Добавлен пропс className в MeshIcon

--- src/entities/node/ui/markerMesh/meshIcon/MeshIcon.js
+++ src/entities/node/ui/markerMesh/meshIcon/MeshIcon.js
@@ -2,15 +2,17 @@ import styles from './MeshIcon.module.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 import {getTagIconComponent} from '../../../../../shared/lib';
+import classNames from 'classnames';
 
 MeshIcon.propTypes = {
+    className: PropTypes.any,
     tag: PropTypes.string,
 };
 
-function MeshIcon({ tag }) {
+function MeshIcon({ className, tag }) {
     let Icon = getTagIconComponent(tag);
 
-    return Icon && <Icon className={styles.meshIcon} />;
+    return Icon && <Icon className={classNames(styles.meshIcon, className)} />;
 }
 
 export default MeshIcon;
