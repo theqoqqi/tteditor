a58b36c9d288a6df7741c99cdfa2614a4100d261
Qoqqi <sazykin.georgii@gmail.com>
2023-08-28 18:11:52 +0300

    feat: В модуль entities/node добавлены экспорты ObjectMesh и MeshIcon

--- src/entities/node/index.js
+++ src/entities/node/index.js
@@ -1,5 +1,9 @@
 import Node from './ui/Node.js';
+import ObjectMesh from './ui/objectMesh/ObjectMesh.js';
+import MeshIcon from './ui/markerMesh/meshIcon/MeshIcon.js';
 
 export {
     Node,
+    ObjectMesh,
+    MeshIcon,
 };
