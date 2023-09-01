3158f6211d06855c1bf3b2fabe319077b6937b26
Qoqqi <sazykin.georgii@gmail.com>
2023-09-01 11:00:15 +0300

    fix: Исправлена ошибка, из-за которой не срабатывали observe-события на поле commands в классе CommandExecutor

--- src/shared/lib/editor/util/CommandExecutor.js
+++ src/shared/lib/editor/util/CommandExecutor.js
@@ -73,7 +73,7 @@ export default class CommandExecutor extends Observable {
     }
 
     clear() {
-        this.commands = [];
+        this.commands.splice(0);
         this.executedCommands = 0;
     }
 
