e83463e8c08fb8ec3e5d1795770901a6bd59b05f
Qoqqi <sazykin.georgii@gmail.com>
2023-09-03 03:07:08 +0300

    feat: В компонент ToolbarSeparator добавлен пропс className

--- src/shared/ui/toolbar/toolbarSeparator/ToolbarSeparator.js
+++ src/shared/ui/toolbar/toolbarSeparator/ToolbarSeparator.js
@@ -1,9 +1,15 @@
 import styles from './ToolbarSeparator.module.css';
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-function ToolbarSeparator() {
+ToolbarSeparator.propTypes = {
+    className: PropTypes.any,
+};
+
+function ToolbarSeparator({ className }) {
     return (
-        <div className={styles.toolbarSeparator} />
+        <div className={classNames(styles.toolbarSeparator, className)} />
     );
 }
 
