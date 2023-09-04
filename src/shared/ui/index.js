f2f3b1d57f95d0d1c6ff6da85fb476bfd86bd3a5
Qoqqi <sazykin.georgii@gmail.com>
2023-09-04 17:30:38 +0300

    refactor: Удален ненужный компонент ToolbarGroup

--- src/shared/ui/index.js
+++ src/shared/ui/index.js
@@ -1,11 +1,10 @@
 import List from './list/List';
-import {ToolbarButton, ToolbarGroup, ToolbarIconButton, ToolbarItem} from './toolbar';
+import {ToolbarButton, ToolbarIconButton, ToolbarItem} from './toolbar';
 import {SidebarTab} from './sidebarTab';
 import {Tabs} from './tabs';
 
 export {
     List,
-    ToolbarGroup,
     ToolbarItem,
     ToolbarButton,
     ToolbarIconButton,
--- src/shared/ui/toolbar/index.js
+++ src/shared/ui/toolbar/index.js
@@ -1,10 +1,8 @@
-import ToolbarGroup from './toolbarGroup/ToolbarGroup';
 import ToolbarItem from './toolbarItem/ToolbarItem';
 import ToolbarButton from './toolbarButton/ToolbarButton';
 import ToolbarIconButton from './toolbarIconButton/ToolbarIconButton';
 
 export {
-    ToolbarGroup,
     ToolbarItem,
     ToolbarButton,
     ToolbarIconButton,
