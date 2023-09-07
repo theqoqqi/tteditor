6d8cc913e09ed070fd0c5b3ee7806dec74821d75
Qoqqi <sazykin.georgii@gmail.com>
2023-09-07 07:11:05 +0300

    feat: Добавлена поддержка nullable-свойств в ObjectEditor

--- src/entities/objectEditor/ui/control/Control.js
+++ src/entities/objectEditor/ui/control/Control.js
@@ -1,18 +1,39 @@
 import styles from './Control.module.css';
-import React from 'react';
+import React, {useCallback} from 'react';
 import PropTypes from 'prop-types';
 import Property from '../property/Property';
+import FormCheckInput from 'react-bootstrap/FormCheckInput';
+import getUniqueProperty from '../../lib/getUniqueProperty';
+
+function isNull(objects, property) {
+    return getUniqueProperty(objects, property.name) === null;
+}
 
 Control.propTypes = {
     title: PropTypes.string,
     objects: PropTypes.arrayOf(PropTypes.object),
     property: PropTypes.shape(Property.propTypes),
     properties: PropTypes.arrayOf(PropTypes.shape(Property.propTypes)),
+    nullable: PropTypes.bool,
     onChange: PropTypes.func,
 };
 
-function Control({ title, objects, property, properties, onChange }) {
+function Control({ title, objects, property, properties, nullable, onChange }) {
     properties ??= [property];
+
+    let onToggleCallback = useCallback(e => {
+        if (e.target.checked) {
+            for (const p of properties) {
+                onChange(p, p.defaultValue ?? '', null, e);
+            }
+        } else {
+            for (const p of properties) {
+                let value = getUniqueProperty(objects, p.name);
+
+                onChange(p, null, value, e);
+            }
+        }
+    }, [objects, onChange, properties]);
 
     return (
         <div className={styles.control}>
@@ -26,8 +47,16 @@ function Control({ title, objects, property, properties, onChange }) {
                         objects={objects}
                         onChange={(...args) => onChange(property, ...args)}
                         {...property}
+                        readonly={property.readonly || (nullable && isNull(objects, property))}
                     />
                 ))}
+                {nullable && (
+                    <FormCheckInput
+                        className={styles.nullCheckbox}
+                        checked={!isNull(objects, properties[0])}
+                        onChange={onToggleCallback}
+                    />
+                )}
             </div>
         </div>
     );
