2960a0eb0e9ad280be666f3047978cb3ba0aac6a
Qoqqi <sazykin.georgii@gmail.com>
2023-09-08 06:59:51 +0300

    feat: Добавлены экспорты для компонентов Control, ControlContainer и PropertyInput

--- src/entities/objectEditor/index.js
+++ src/entities/objectEditor/index.js
@@ -1,8 +1,14 @@
 import ObjectEditor from './ui/ObjectEditor';
 import getUniqueProperty, {differentValues} from './lib/getUniqueProperty';
+import Control from './ui/control/Control';
+import ControlContainer from './ui/controlContainer/ControlContainer';
+import PropertyInput from './ui/propertyInput/PropertyInput';
 
 export {
     ObjectEditor,
+    Control,
+    ControlContainer,
+    PropertyInput,
     getUniqueProperty,
     differentValues,
 };
