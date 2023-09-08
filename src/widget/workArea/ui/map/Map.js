973e4e785d092801769700b8fe4f333a88798644
Qoqqi <sazykin.georgii@gmail.com>
2023-09-08 20:45:37 +0300

    feat: Теперь Map отслеживает позицию курсора и хранит кк в pointerPositionSlice

--- src/widget/workArea/ui/map/Map.js
+++ src/widget/workArea/ui/map/Map.js
@@ -1,20 +1,25 @@
 import styles from './Map.module.css';
-import React, {useCallback} from 'react';
+import React, {useCallback, useEffect, useRef} from 'react';
 import NodeList from './nodeList/NodeList';
 import {selectSelectedMapNodes} from '../../../../entities/selection';
 import {useMap, useMapObserver} from '../../../../shared/lib';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {actions, useSelectMapNodeCallback} from '../../../../features/selection';
 import useArrowMovement from '../../lib/useArrowMovement';
 import useDragMovement from '../../lib/useDragMovement';
 import Terrain from './terrain/Terrain';
 import {isHotkeyPressed} from 'react-hotkeys-hook';
 import useFocusNodeListAtId from '../../lib/useFocusNodeListAtId';
+import {useMouse} from 'react-use';
+import {setPointerPositionOnMap} from '../../../../entities/pointerPosition';
 
 function Map() {
+    let ref = useRef(null);
     let map = useMap();
     let terrain = useMapObserver('terrain');
     let selectedMapNodes = useSelector(selectSelectedMapNodes);
+    let mouse = useMouse(ref);
+    let dispatch = useDispatch();
     let resolveAction = useCallback(() => {
         if (isHotkeyPressed('shift')) {
             return actions.add;
@@ -35,6 +40,13 @@ function Map() {
     useMapObserver('width');
     useMapObserver('height');
 
+    useEffect(() => {
+        dispatch(setPointerPositionOnMap({
+            x: mouse.elX,
+            y: mouse.elY,
+        }));
+    }, [dispatch, mouse.elX, mouse.elY]);
+
     if (!map) {
         return null;
     }
@@ -45,7 +57,7 @@ function Map() {
     };
 
     return (
-        <div className={styles.map} style={style}>
+        <div ref={ref} className={styles.map} style={style}>
             <Terrain terrain={terrain} />
             <NodeList
                 selectedMapNodes={selectedMapNodes}
