1a3c1d32236c68296b4149f4c27f27489d1663a9
Qoqqi <sazykin.georgii@gmail.com>
2023-09-10 02:47:24 +0300

    refactor: Компонент NodeList в виджете WorkArea теперь использует хук useIsMapNodeVisible

--- src/widget/workArea/ui/map/nodeList/NodeList.js
+++ src/widget/workArea/ui/map/nodeList/NodeList.js
@@ -1,12 +1,10 @@
 import styles from './NodeList.module.css';
 import React from 'react';
 import PropTypes from 'prop-types';
-import {selectVisibleLayers} from '../../../../../entities/layers';
 import {Node} from '../../../../../entities/node';
-import {useSelector} from 'react-redux';
-import {EditorContext, MapNode, useMapListObserver} from '../../../../../shared/lib';
+import {MapNode, useMapListObserver} from '../../../../../shared/lib';
 import {usePointerMode} from '../../../../../entities/pointerMode';
-import {selectHiddenMapNodes} from '../../../../../entities/hiddenNodes';
+import useIsMapNodeVisible from '../../../lib/useIsMapNodeVisible';
 
 NodeList.propTypes = {
     selectedMapNodes: PropTypes.arrayOf(PropTypes.instanceOf(MapNode)),
@@ -17,32 +15,25 @@ NodeList.propTypes = {
 
 function NodeList({ selectedMapNodes, onClickMapNode, onPointerDown, onDoubleClick }) {
     let [mapNodes] = useMapListObserver('nodes');
-    let visibleLayers = useSelector(selectVisibleLayers);
-    let hiddenMapNodes = useSelector(selectHiddenMapNodes);
+    let isMapNodeVisible = useIsMapNodeVisible();
     let pointerMode = usePointerMode();
 
     let isSelected = mapNode => pointerMode.canSelectNodes && selectedMapNodes?.includes(mapNode);
 
     return (
         <div className={styles.mapNodeList}>
-            {mapNodes?.map(mapNode => {
-                let layerName = EditorContext.getLayerNameByTagName(mapNode.tag);
-                let isLayerVisible = visibleLayers.includes(layerName);
-                let isHidden = hiddenMapNodes.includes(mapNode);
-
-                return (
-                    <Node
-                        key={mapNode.editorId}
-                        mapNode={mapNode}
-                        selected={isSelected(mapNode)}
-                        hidden={isHidden || !isLayerVisible}
-                        interactable={pointerMode.canSelectNodes}
-                        onClick={onClickMapNode}
-                        onPointerDown={onPointerDown}
-                        onDoubleClick={onDoubleClick}
-                    />
-                );
-            })}
+            {mapNodes?.map(mapNode => (
+                <Node
+                    key={mapNode.editorId}
+                    mapNode={mapNode}
+                    selected={isSelected(mapNode)}
+                    hidden={!isMapNodeVisible(mapNode)}
+                    interactable={pointerMode.canSelectNodes}
+                    onClick={onClickMapNode}
+                    onPointerDown={onPointerDown}
+                    onDoubleClick={onDoubleClick}
+                />
+            ))}
         </div>
     );
 }
