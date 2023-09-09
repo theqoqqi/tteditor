6bd545199d16ecf15b4c1ed123f9b56b90514916
Qoqqi <sazykin.georgii@gmail.com>
2023-09-09 23:57:35 +0300

    feat: В shared-компонент Button добавлен пропс hotkey

--- src/shared/ui/button/Button.js
+++ src/shared/ui/button/Button.js
@@ -2,18 +2,35 @@ import styles from './Button.module.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import {useHotkeys} from 'react-hotkeys-hook';
+
+function Hotkey({ hotkey, action, disabled }) {
+    useHotkeys(hotkey, e => {
+        if (disabled) {
+            return;
+        }
+
+        e.preventDefault();
+        e.stopPropagation();
+
+        action(e);
+    });
+
+    return null;
+}
 
 Button.propTypes = {
     className: PropTypes.any,
     children: PropTypes.any,
     title: PropTypes.string,
+    hotkey: PropTypes.string,
     toggle: PropTypes.bool,
     active: PropTypes.bool,
     disabled: PropTypes.bool,
     onClick: PropTypes.func,
 };
 
-function Button({ className, children, title, toggle, active, disabled, onClick }) {
+function Button({ className, children, title, hotkey, toggle, active, disabled, onClick }) {
     return (
         <div
             title={title}
@@ -24,6 +41,7 @@ function Button({ className, children, title, toggle, active, disabled, onClick 
             })}
             onClick={e => disabled ? null : onClick(e)}
         >
+            {hotkey && <Hotkey hotkey={hotkey} action={onClick} disabled={disabled} />}
             {children}
         </div>
     );
