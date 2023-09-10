3f77c20d1a893b4fe6ec4b750740e2c1306bb64b
Qoqqi <sazykin.georgii@gmail.com>
2023-09-10 06:08:02 +0300

    perf: Теперь элементы палитры не скрываются (visibility: hidden), если находятся за пределами прокрутки и не видны

--- src/features/palette/item/ui/paletteItemPreview/PaletteItemPreview.js
+++ src/features/palette/item/ui/paletteItemPreview/PaletteItemPreview.js
@@ -5,6 +5,8 @@ import {useFirstIntersection} from '../../../../../shared/lib';
 import {MapNode} from '../../../../../shared/lib';
 import TerrainPreview from './terrainPreview/TerrainPreview';
 import NodesPreview from './nodesPreview/NodesPreview';
+import {useIntersection} from 'react-use';
+import classNames from 'classnames';
 
 PaletteItemPreview.propTypes = {
     tag: PropTypes.string,
@@ -23,13 +25,21 @@ function createContent(tag, type, mapNodes) {
 function PaletteItemPreview({ tag, type, mapNodes }) {
     let [content, setContent] = useState(null);
     let ref = useRef(null);
+    let intersection = useIntersection(ref, {});
 
     useFirstIntersection(ref, () => {
         setContent(createContent(tag, type, mapNodes));
     }, [tag, type, mapNodes]);
 
+    let isVisible = intersection && intersection.isIntersecting;
+
     return (
-        <div ref={ref} className={styles.paletteItemPreview}>
+        <div
+            ref={ref}
+            className={classNames(styles.paletteItemPreview, {
+                [styles.hidden]: !isVisible,
+            })}
+        >
             {content}
         </div>
     );
